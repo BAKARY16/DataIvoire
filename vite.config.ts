b4f3da3b7a94c4951101a60079a62b1d8d92bca1
BAKARY16 <s.bakary1611@gmail.com>
2025-06-24 18:53:49 +0000

    Mise a jour

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  /*base: '/Dataivoire/', */// Remplacez "project-name" par le nom de votre projet GitLab // Remplacez "project-name" par le nom de votre projet GitLab
+  base: '/DataIvoire/', // Remplacez "project-name" par le nom de votre projet GitLab // Remplacez "project-name" par le nom de votre projet GitLab
   plugins: [react()],
 });
